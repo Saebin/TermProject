44afe40d2602702d98fac5b0adbb95525e2dbaa1
S_se <tpqlsgkgk@gmail.com>
2016-11-30 19:27:04 +0900

    추가

--- app/src/main/java/com/example/termproject/DetailActivity.java
+++ app/src/main/java/com/example/termproject/DetailActivity.java
@@ -16,7 +16,7 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         intent.getStringExtra("date");
-        
+
 
     }
 }
--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.termproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String a;
-    
+    CalendarView calender;
+    String date;
+    Button detailBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calender = (CalendarView) findViewById(R.id.calendarView);
+        detailBtn = (Button) findViewById(R.id.detail);
+
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                date = year + "년 " + (month + 1) + "월" + dayOfMonth + "일";
+            }
+
+        });
+
+        detailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("date", date);
+                startActivityForResult(intent, Integer.parseInt("0"));
+            }
+        });
+
     }
 
 }
